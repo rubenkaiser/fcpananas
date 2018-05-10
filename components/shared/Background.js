3e1bb10c626d9346157eb2b30e019af3e748a2e7
Ruben Kaiser <Ruben@C02RJ36JG8WP.local>
2018-05-10 19:03:21 +0200

    added docs to gitignore and updated components with image refs

--- components/shared/Background.js
+++ components/shared/Background.js
@@ -12,7 +12,7 @@ const styles = theme => ({
     zIndex: -1,
     minHeight: '100vh',
     backgroundSize: 'cover',
-    backgroundImage: 'url(/static/bg.jpg)'
+    backgroundImage: `url(${process.env.ASSET_PREFIX}/static/bg.jpg)`
   },
 });
 
